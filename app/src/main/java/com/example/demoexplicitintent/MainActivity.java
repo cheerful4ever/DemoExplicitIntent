e0b71b94844e0eb4d945bc7fb57ed0b5604b3ad5
18016375 <18016375@myrp.edu.sg>
2020-05-08 12:03:34 +0800

    activities 1-1000

--- app/src/main/java/com/example/demoexplicitintent/MainActivity.java
+++ app/src/main/java/com/example/demoexplicitintent/MainActivity.java
@@ -6,8 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    // These request identify who started the second activity
+    int requestCodeForSupermanStats = 1;
+    int requestCodeForBatmanStats = 2;
 
     TextView tvSuperman, tvBatman;
 
@@ -29,7 +34,12 @@ public class MainActivity extends AppCompatActivity {
                         HeroStatsActivity.class);
                 // Put hero object in intent
                 i.putExtra("hero", superman);
-                startActivity(i);
+                //startActivity(i);
+
+                // Start activity with requestCodeForSupermanStats to
+                // identify it was started by clicking on Superman
+                startActivityForResult(i,requestCodeForSupermanStats);
+
             }
         });
 
@@ -44,8 +54,41 @@ public class MainActivity extends AppCompatActivity {
                 // Put hero object in intent
                 i.putExtra("hero", batman);
                 // Start the activity
-                startActivity(i);
+                //startActivity(i);
+
+                // Start activity with requestCodeForBatmanStats to
+                // identify started by clicking Batman
+                startActivityForResult(i, requestCodeForBatmanStats);
+
             }
         });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int 				resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Only handle when 2nd activity closed normally
+        //  and data contains something
+        if(resultCode == RESULT_OK){
+            if (data != null) {
+                // Get data passed back from 2nd activity
+                String like = data.getStringExtra("like");
+                String statement = "";
+                // If it is activity started by clicking 				//  Superman, create corresponding String
+                if(requestCode == requestCodeForSupermanStats){
+                    statement = "You " + like + " Superman";
+                }
+                // If 2nd activity started by clicking
+                //  Batman, create a corresponding String
+                if(requestCode == requestCodeForBatmanStats){
+                    statement = "You " + like + " Batman";
+                }
+
+                Toast.makeText(MainActivity.this, statement,
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
 }
